12986d6c3e8250825cc16632d4185c93c1540b00
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-30 19:02:58 -0500

    css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
 import sergioAltuzarLogo from './imagenes/logo-sergio-altuzar.png';
 
 function App() {
+  const manejarClic = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
   return (
     <div className="App">
       <div className='logo-contenedor' >
@@ -11,7 +20,16 @@ function App() {
         alt='logo sergio'/>
       </div>
       <div className='contenedor-principal'>
-        
+        <Contador
+            numClics='5' />
+        <Boton
+        texto='Clic'
+        esBotonDeClic={true} 
+        manejarClic={manejarClic} />
+        <Boton
+         texto='Reiniciar'
+         esBotonDeClic={false} 
+         manejarClic={reiniciarContador} />
 
       </div>
     </div>
